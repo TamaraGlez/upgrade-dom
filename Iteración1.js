c93b906eae4a5256ae2ac3bf7638cb68982fc5de
TamaraGlez <tamara.gonzalez@bootcamp-upgrade.com>
2022-12-19 16:40:15 +0100

    Iteración1

---
+++ Iteración1.js
@@ -0,0 +1,14 @@
+// 1.1 Usa querySelector para mostrar por consola el botón con la clase .**showme**
+// const classimput = document.getElementsByClassName("showme")
+// console.log(classimput)
+const classhowme = document.querySelectorAll(".showme");
+console.log(classhowme);
+// 1.2 Usa querySelector para mostrar por consola el h1 con el id #pillado #para indicar que es un id
+const idPillado = document.querySelector("#pillado");
+console.log(idPillado);
+// 1.3 Usa querySelector para mostrar por consola todos los p
+const allP = document.querySelectorAll("p");
+console.log(allP);
+// 1.4 Usa querySelector para mostrar por consola todos los elementos con la clase.pokemon
+const allClassPokemon = document.querySelectorAll('.pokemon')
+console.log(allClassPokemon);
